f4608b7e9a6c7fb39c2c6dc04655118a7ba0c29a
Aramata123 <164217844+Aramata123@users.noreply.github.com>
2025-04-14 14:46:27 +0200

    ajustement paragraphe

--- resources/views/vet.blade.php
+++ resources/views/vet.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="row no-gutters slider-text align-items-end">
                 <div class="col-md-9 ftco-animate pb-5">
-                    <p class="breadcrumbs mb-2"><span class="mr-2"><a href="/">Accueil <i class="ion-ios-arrow-forward"></i></a></span> <span>Veterinaire <i class="ion-ios-arrow-forward"></i></span></p>
+                    <p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Accueil <i class="ion-ios-arrow-forward"></i></a></span> <span>Veterinarian <i class="ion-ios-arrow-forward"></i></span></p>
                     <h1 class="mb-0 bread">Rencontrez nos vétérinaires</h1>
                 </div>
             </div>
@@ -22,11 +22,11 @@
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
                             <h3>Lucas Moreau</h3>
-                            <span class="position mb-2">Vétérinaire comportementaliste</span>
+                            <span class="position mb-2">Troubles animaux</span>
                             <div class="faded">
-                                <p>J'aide les animaux à surmonter leurs troubles du comportement (anxiété, agressivité, peurs) avec des méthodes douces et personnalisées.</p>
+                                <p>J'aide les animaux anxieux ou agressifs à retrouver un comportement équilibré avec une approche douce et adaptée à leur nature.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -39,9 +39,9 @@
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
                             <h3>Claire Dupont</h3>
-                            <span class="position mb-2">Spécialiste en soins préventifs et bien-être animal</span>
+                            <span class="position mb-2">Santé bien-être animal</span>
                             <div class="faded">
-                                <p>Passionnée par les animaux depuis toujours, j'exerce la médecine vétérinaire avec rigueur et bienveillance depuis plus de 10 ans. </p>
+                                <p>Je propose des soins préventifs et un suivi personnalisé pour améliorer le bien-être de vos animaux de compagnie au quotidien.</p>
                                 <ul class="ftco-social text-center">
                                     <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
@@ -56,11 +56,11 @@
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
                             <h3>Julia Martin</h3>
-                            <span class="position mb-2">Vétérinaire généraliste – Soins des animaux de compagnie</span>
+                            <span class="position mb-2">Soins compagnons</span>
                             <div class="faded">
-                                <p>Empathique et douce, Claire accompagne les familles et leurs animaux avec une attention particulière portée à la prévention et au bien-être au quotidien.</p>
+                                <p>Je soigne vos animaux de compagnie avec empathie et professionnalisme, en assurant leur confort et une bonne santé globale durable.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -72,30 +72,29 @@
                             <div class="img align-self-stretch" style="background-image: url(images/staff-4.jpg);"></div>
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
-                            <h3> Dr. Romain Chevalier</h3>
-                            <span class="position mb-2">Vétérinaire dentiste – Spécialiste en soins bucco-dentaires</span>
+                            <h3>Dr. Romain Chevalier</h3>
+                            <span class="position mb-2">Santé bucco-dentaire</span>
                             <div class="faded">
-                                <p>Romain intervient sur les problèmes dentaires des chiens, chats et NAC. Son expertise permet d’éviter de nombreuses douleurs invisibles mais fréquentes.</p>
+                                <p>J'effectue les soins dentaires essentiels pour éviter douleurs, infections et améliorer la qualité de vie de vos compagnons.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <div class="col-md-6 col-lg-3 ftco-animate">
                     <div class="staff">
                         <div class="img-wrap d-flex align-items-stretch">
                             <div class="img align-self-stretch" style="background-image: url(images/staff-5.jpg);"></div>
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
-                            <h3> Dr. Camille Fontaine</h3>
-                            <span class="position mb-2">Vétérinaire aviaire – Soins des oiseaux domestiques et exotiques</span>
+                            <h3>Dr. Camille Fontaine</h3>
+                            <span class="position mb-2">Soins oiseaux exotiques</span>
                             <div class="faded">
-                                <p>Camille est passionnée par les perroquets, canaris et autres oiseaux tropicaux. j'allie connaissances médicales et conseils en comportement aviaire.</p>
+                                <p>J'apporte des soins dédiés aux oiseaux domestiques et tropicaux, avec une attention particulière au comportement et à la santé des concerner.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -108,11 +107,11 @@
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
                             <h3>Dr. Adrien Moreau</h3>
-                            <span class="position mb-2">Vétérinaire urgentiste – Clinique 24h/24</span>
+                            <span class="position mb-2">Urgences animales</span>
                             <div class="faded">
-                                <p>Disponible jour et nuit, j'intervient en urgence pour les cas critiques : accidents, empoisonnements, chirurgies d’urgence.</p>
+                                <p>Je gère les urgences animales 24h/24 : traumatismes, empoisonnements, chirurgies vitales et soins intensifs.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -125,11 +124,11 @@
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
                             <h3>Hugo Dubois</h3>
-                            <span class="position mb-2">Vétérinaire généticienne – Reproduction & sélection</span>
+                            <span class="position mb-2">Vétérinaire généticien</span>
                             <div class="faded">
-                                <p>Isabelle travaille en lien avec des éleveurs pour assurer une reproduction saine et responsable. j'accompagne aussi les familles dans les choix liés à la stérilisation ou la prévention des maladies héréditaires.</p>
+                                <p>Je conseille sur la reproduction animale responsable et préviens les maladies génétiques héréditaires chez vos animaux.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -141,12 +140,12 @@
                             <div class="img align-self-stretch" style="background-image: url(images/staff-8.jpg);"></div>
                         </div>
                         <div class="text pt-3 px-3 pb-4 text-center">
-                            <h3> Dr. Stéphane Lemoine</h3>
-                            <span class="position mb-2">Vétérinaire neurologue – Troubles du système nerveux</span>
+                            <h3>Dr. Stéphane Lemoine</h3>
+                            <span class="position mb-2">Vétérinaire neurologue</span>
                             <div class="faded">
-                                <p>Avec une approche ultra spécialisée, je prend en charge les animaux souffrant de troubles moteurs, convulsions ou pathologies rares du cerveau et de la moelle épinière.</p>
+                            <p>Je traite les troubles nerveux tels que convulsions, paralysies, pertes d’équilibre ou comportements anormaux chez vos animaux domestiques.</p>
                                 <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-comment"></span></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -155,85 +154,4 @@
             </div>
         </div>
     </section>
-
-    <section class="ftco-appointment ftco-section ftco-no-pt ftco-no-pb img" style="background-image: url(images/bg_3.jpg);">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row d-md-flex justify-content-end">
-                <div class="col-md-12 col-lg-6 half p-3 py-5 pl-lg-5 ftco-animate">
-                    <h2 class="mb-4">Consultation</h2>
-                    <form action="{{ route('subscription.create') }}" method="POST" class="appointment">
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <div class="form-field">
-                                        <div class="select-wrap">
-                                            <div class="icon"><span class="fa fa-chevron-down"></span></div>
-                                            <select name="" id="" class="form-control">
-                                            <option value="">Sélectionnez un service</option>
-                                            <option value="consultation-generale">Consultation générale</option>
-                                            <option value="soins-preventifs">Soins préventifs & bilans de santé</option>
-                                            <option value="comportement-animal">Consultation comportementale</option>
-                                            <option value="urgence">Consultation d'urgence 24h/24</option>
-                                            <option value="neurologie">Consultation neurologique</option>
-                                            <option value="dentisterie">Soins bucco-dentaires</option>
-                                            <option value="nac-aviaire">Soins pour oiseaux et NAC</option>
-                                            <option value="reproduction">Suivi reproduction & génétique</option>
-                                            <option value="vaccination">Vaccination & rappels</option>
-                                            <option value="conseil-bien-etre">Conseils en bien-être animal</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Votre Nom">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Telephone">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <div class="input-wrap">
-                                        <div class="icon"><span class="fa fa-calendar"></span></div>
-                                        <input type="text" class="form-control appointment_date" placeholder="Date">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <div class="input-wrap">
-                                        <div class="icon"><span class="fa fa-clock-o"></span></div>
-                                        <input type="text" class="form-control appointment_time" placeholder="Heure">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <input type="submit" value="Envoyer" class="btn btn-primary py-3 px-4">
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- loader -->
-    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
-        </svg></div>
-
 </x-layout>
